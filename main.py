2a99770355ff688ac868f3cd041059ef20248da6
Raphael Oliveira Cunha <raphael.o.cunha@hotmail.com>
2025-02-11 18:28:11 -0300

    Codificação para a abertura do arquivo .json

--- main.py
+++ main.py
@@ -15,35 +15,40 @@ def index():
 
 @app.route("/semana", methods=["GET"])
 def semana():
-    with open(path.join("src", "semana.json"), "r") as arquivo:
+    with open(path.join("src", "semana.json"), "r", encoding="utf8")\
+            as arquivo:
         semana = json.load(arquivo)
         return json.dumps(semana, ensure_ascii=False).encode("utf8")
 
 
 @app.route("/estados", methods=["GET"])
 def estados():
-    with open(path.join("src", "estados.json"), "r") as arquivo:
+    with open(path.join("src", "estados.json"), "r", encoding="utf8")\
+            as arquivo:
         estados = json.load(arquivo)
         return json.dumps(estados, ensure_ascii=False).encode("utf8")
 
 
 @app.route("/estados_select", methods=["GET"])
 def estados_select():
-    with open(path.join("src", "estados_uf.json"), "r") as arquivo:
+    with open(path.join("src", "estados_uf.json"), "r", encoding="utf8")\
+            as arquivo:
         estados = json.load(arquivo)
         return json.dumps(estados, ensure_ascii=False).encode("utf8")
 
 
 @app.route("/regioes", methods=["GET"])
 def regioes():
-    with open(path.join("src", "regioes.json"), "r") as arquivo:
+    with open(path.join("src", "regioes.json"), "r", encoding="utf8")\
+            as arquivo:
         regioes = json.load(arquivo)
         return json.dumps(regioes, ensure_ascii=False).encode("utf8")
 
 
 @app.route("/cardeais", methods=["GET"])
 def cardeais():
-    with open(path.join("src", "pontos_cardeais.json"), "r") as arquivo:
+    with open(path.join("src", "pontos_cardeais.json"), "r", encoding="utf8")\
+            as arquivo:
         cardeais = json.load(arquivo)
         return json.dumps(cardeais, ensure_ascii=False).encode("utf8")
 
